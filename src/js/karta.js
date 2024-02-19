0332c6d77890d0d2b4198a4c090518baf408dae8
Jocke <joakim03moller@gmail.com>
2024-02-19 10:36:11 +0100

    Karta avklarad

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,21 +1,34 @@
 "use strict";
-
-
+/* Hämtar sökknappen samt skapar en eventlistenet för värdet på input. Skickar med value till nästa funktion */
 const searchBtn = document.getElementById("searchBtn");
-
 searchBtn.addEventListener("click", function () {
-    const searchInp = document.getElementById("searchInp").value;
-    console.log(searchInp);
-    getData(searchInp);
-    
-})
+  const searchInp = document.getElementById("searchInp").value;
+  getData(searchInp);
+});
 
+/* Fetchar data med value från eventlistener. Skickar data till cordMap. */
 async function getData(searchVal) {
-    try {
-        const response = await fetch('https://nominatim.openstreetmap.org/search?addressdetails=1&q=' + searchVal + '&format=jsonv2&limit=1');
-        let data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Kunde inte fetcha, följande felmeddelande skapades:', error);
-    }
+  try {
+    const response = await fetch(
+      "https://nominatim.openstreetmap.org/search?addressdetails=1&q=" +
+        searchVal +
+        "&format=jsonv2&limit=1"
+    );
+    let data = await response.json();
+    cordMap(data);
+  } catch (error) {
+    console.error("Kunde inte fetcha, följande felmeddelande skapades:", error);
+  }
+}
+
+/* Funktion för att få fram kordinaterna i variabler, för att på så sätt ändra innehåll i iframen. */
+function cordMap(cords) {
+    let longitude = cords[0].lon;
+    let latitude = cords[0].lat;
+    let boxLatitude1 = cords[0].boundingbox[0];
+    let boxLatitude2 = cords[0].boundingbox[1];
+    let boxLongitude1 = cords[0].boundingbox[2];
+    let boxLongitude2 = cords[0].boundingbox[3];
+    /* Hämtar element och ändrar SRC-länken med rätt data */
+    document.getElementById("map").src = ("https://www.openstreetmap.org/export/embed.html?bbox=" + boxLongitude1 + "%2C" + boxLatitude1 + "%2C" + boxLongitude2 + "%2C" + boxLatitude2 + "&layer=mapnik&marker=" + latitude + "%2C" + longitude);
 }
